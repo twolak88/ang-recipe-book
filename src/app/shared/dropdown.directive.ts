0b7e9a2260ffddd90a23b3671116f181f5b6cb7a
twolak88 <twolak88@gmail.com>
2021-02-09 00:54:36 +0100

    Adding Dropdown directive and using it on buttons

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  constructor() { }
+
+  @HostListener('click') toggleOpen(eventData: Event) {
+    this.isOpen = !this.isOpen;
+  }
+}
