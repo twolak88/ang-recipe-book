e4200327a7ab52c2b6c5c31a3a2e1fdc6a1f8c3e
twolak88 <twolak88@gmail.com>
2021-02-15 21:42:08 +0100

    Reflecting the Auth State in the UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService, AuthResponseData } from './services/auth.service';
 
@@ -14,7 +15,8 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -38,6 +40,7 @@ export class AuthComponent implements OnInit {
       responseData => {
         console.log(responseData);
         this.isLoading = false;
+        this.router.navigate(['/']);
       }, errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
 import { DataStorageService } from '../shared/services/data-storage.service';
 
 @Component({
@@ -6,8 +8,22 @@ import { DataStorageService } from '../shared/services/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authUserSub: Subscription;
+  isAuthenticated: boolean = false;
+
+  constructor(private dataStorageService: DataStorageService,
+    private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authUserSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user; //!user ? false : true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authUserSub.unsubscribe();
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
