faf8abd3bc2969010f12e2ddfdfedc23b05f092b
twolak88 <twolak88@gmail.com>
2021-02-17 19:51:43 +0100

    Adding Action, Reduler and Store for shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,9 @@ import { CoreModule } from './core.module';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({
+      shoppingList: shoppingListReducer
+    }),
     AppRoutingModule,
     SharedModule,
     CoreModule
