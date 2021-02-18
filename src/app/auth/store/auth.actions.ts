81f8ef75170be59ce5fcd98c498c000561cd89c4
twolak88 <twolak88@gmail.com>
2021-02-18 19:20:20 +0100

    Refactor Actions types

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[AUTH] Login';
+export const LOGOUT = '[AUTH] Logout';
 
 export class Login implements Action {
   readonly type = LOGIN;
