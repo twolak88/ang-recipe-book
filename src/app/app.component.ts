be2c09a0a2d2b5613c9a072f62c9bccd6e4a30c9
twolak88 <twolak88@gmail.com>
2021-03-04 20:41:06 +0100

    Adding ';'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as AuthActions from './auth/store/auth.actions';
-import * as fromApp from './store/app.reducer'
+import * as fromApp from './store/app.reducer';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ import * as fromApp from './store/app.reducer'
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store<fromApp.AppState>,
-    @Inject(PLATFORM_ID) private platformId) {}
+              @Inject(PLATFORM_ID) private platformId) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
